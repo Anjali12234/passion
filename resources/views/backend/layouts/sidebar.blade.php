c53d4243ba291b67368b5213a695c4e991498e8b
anjali sharma <anjisharma649@gmail.com>
2023-12-30 13:44:18 +0545

    first commit

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="left-side-bar">
     <div class="brand-logo">
         <a href="{{ route('admin.dashboard') }}">
-            <img src="{{ officeSetting()->institute_logo }}" alt="" class="dark-logo" />
-            <img src="{{ officeSetting()->institute_logo }}" alt="" class="light-logo" />
+            <img src="{{ officeSetting()->institute_logo ??'' }}" alt="" class="dark-logo" />
+            <img src="{{ officeSetting()->institute_logo ??''}}" alt="" class="light-logo" />
         </a>
         <div class="close-sidebar" data-toggle="left-sidebar-close">
             <i class="ion-close-round"></i>
